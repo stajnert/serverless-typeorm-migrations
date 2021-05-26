b125d055ebda187573710ce25ab41f0ff8d68ee6
Sławek Tajnert <stajnert@mac.local>
2021-05-26 12:08:41 +0200

    remove build

--- build/handlers.d.ts
+++
@@ -1,4 +0,0 @@
-import { Context, Callback } from "aws-lambda";
-declare const up: (event: any, context: Context, callback: Callback<any>) => Promise<void>;
-declare const down: (event: any, context: Context, callback: Callback<any>) => Promise<void>;
-export { up, down };
--- build/utils.d.ts
+++
@@ -1,5 +0,0 @@
-import { Migration } from "typeorm";
-export declare function getDatabaseConnectionString(logger: (message: string) => void): string;
-export declare function getEngine(logger: (message: string) => void): any;
-export declare function getMigrationFolder(logger: (message: string) => void): string;
-export declare function logMigrations(logger: (message: string) => void, migrations: Migration[] | undefined): void;
